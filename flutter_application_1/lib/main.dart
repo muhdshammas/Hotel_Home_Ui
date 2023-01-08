ae5f79592c12622ef06f0a27f8ef30c4cfb668cd
muhdshammas <shammasa03@gmail.com>
2023-01-08 22:58:48 +0530

    commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+import 'Home.dart';
+import 'details.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+        builder: (context, orientation, deviceType) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/',
+            routes: {
+              '/' : (context) => Home(),
+              '/Deatils' : (context) => Deatils(),
+
+            },
+
+          );
+        }
+    );
+  }
+}
